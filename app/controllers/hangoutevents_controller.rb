ca8c61192bcb979452e795723ffc9f1f875f03f0
thehme <bcortez23@gmail.com>
2014-10-19 11:19:23 -0400

    added edit method

--- app/controllers/hangoutevents_controller.rb
+++ app/controllers/hangoutevents_controller.rb
@@ -4,12 +4,15 @@ class HangouteventsController < ApplicationController
 	end
 
 	def new
+	  # binding.pry
 	  @event = Hangoutevent.new
 	end
 
 	def create
-		# Hangoutevent.create(params[:hangoutevent])
-		Hangoutevent.create(hangoutevent_params)
+		# binding.pry
+		Hangoutevent.create(params[:hangoutevent])
+
+		#Hangoutevent.create(hangoutevent_params)
 		redirect_to hangoutevents_path
 	end
 
@@ -19,10 +22,14 @@ class HangouteventsController < ApplicationController
 	def destroy
 	end
 
+	def edit
+		
+	end
+
 	private
 
-	def hangoutevent_params
-		params.require(:hangoutevent).permit(:title, :start_date, :start_date_time, :description)
-		# params.permit(:title, :start_date_day, :start_date_time, :description)
-	end
+	# def hangoutevent_params
+	# 	params.require(:hangoutevent).permit(:title, :start_date, :start_date_time, :description)
+	# 	# params.permit(:title, :start_date_day, :start_date_time, :description)
+	# end
 end
